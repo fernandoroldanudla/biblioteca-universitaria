709f0be1c90755a45cdc77f2c273b0debd0f6019
fernandoroldanudla <fernando.roldan@udla.edu.ec>
2026-05-21 21:10:48 -0500

    Add publication year to Libro instantiation in tests
    Updated Libro instantiation to include publication year.

--- biblioteca-universitaria/src/test/java/edu/universidad/biblioteca/LibroTest.java
+++ biblioteca-universitaria/src/test/java/edu/universidad/biblioteca/LibroTest.java
@@ -7,14 +7,14 @@ public class LibroTest {
 
     @Test
     void testPrestarLibro() {
-        Libro libro = new Libro("1984", "Orwell");
+        Libro libro = new Libro("1984", "Orwell", 1984);
         libro.prestar();
         assertFalse(libro.estaDisponible());
     }
 
     @Test
     void testPrestarLibroNoDisponible() {
-        Libro libro = new Libro("1984", "Orwell");
+        Libro libro = new Libro("1984", "Orwell", 1984);
         libro.prestar();
         assertThrows(IllegalStateException.class, libro::prestar);
     }
